abb3523acd5f3cbfd382e3bd639f414707af655a
Matt Searle <matthewsearle01@gmail.com>
2021-10-12 16:04:28 +0100

    removed redundant line from js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Function to hide zombies on click.
-const allZombies = document.querySelectorAll('.zombie-sprite');
 
 $('.zombie-sprite').on('click', function(){
     $(this).css('display', 'none');
