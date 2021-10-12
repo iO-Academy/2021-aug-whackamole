c1a068b7efbf33a7e76b829148d0a87d408a96c2
Ben <benjamindhayden@gmail.com>
2021-10-12 11:30:22 +0100

    fixed css/html and removed some js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,6 @@
-// hideZombie = (ev) => {
-//     const allZombies = document.querySelector('#z1');
-//     allZombies.setAttribute('display', 'none');
-// }
-//
-// clickHandle = () => {
-//     const allZombies = document.querySelectorAll('#z1');
-//     allZombies.addEventListener('click', hideZombie());
-// }
-// const allZombies = document.querySelectorAll('.zombie-hitbox');
-// $('allZombies').on('click', function(){
-//     $(this).css('display', 'none');
-// });
-
-// clickHandle();
+// Function to hide zombies on click.
 const allZombies = document.querySelectorAll('.zombie-sprite');
+
 $('.zombie-sprite').on('click', function(){
     $(this).css('display', 'none');
 });
-// allZombies.forEach(zombie => {
-//     zombie.addEventListener('click', handleClickMeClick());
-// })
