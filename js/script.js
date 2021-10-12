92594c855e3505db13ebfad54e8446c1a9851c13
Henri <henridvf@gmail.com>
2021-10-12 14:58:07 +0100

    Deleted unused querySelectorAll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 
 // Function to hide zombies on click.
-const allZombies = document.querySelectorAll('.zombie-sprite');
-
 $('.zombie-sprite').on('click', function(){
     $(this).css('display', 'none');
     addToScore();
