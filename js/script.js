0d0030199c839d82c6603ef9b23bf03ae0a0d121
Judy <judyelewisjones@hotmail.co.uk>
2021-10-13 15:19:35 +0100

    writing timer function

--- js/script.js
+++ js/script.js
@@ -57,3 +57,27 @@ $(document).ready(function() {
         $(".info-circle").toggle();
     });
 });
+
+// 30 second timer
+let timer;
+let timeLeft = 30; // seconds
+
+// decrements time left by one if time left is greater than zero.
+
+function updateTimer() {
+    timeLeft = timeLeft - 1;
+    if (timeLeft >= 0)
+        $('#timer').html(timeLeft);
+    else {
+        endGame();
+    }
+}
+
+function startGame() {
+    timer = setInterval(updateTimer, 1000); // 1000 = 1second
+}
+
+function endGame() {
+    cancelInterval(timer);
+}
+
